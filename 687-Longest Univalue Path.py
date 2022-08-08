840949c9dafe627e32e820577571e13d70623c05
Lawrence Tsai <l.tsai@mail.utoronto.ca>
2022-08-07 23:16:51 -0600

    Create 687-Longest Univalue Path.py

---
+++ 687-Longest Univalue Path.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+	max_path=0
+	def longestUnivaluePath(self, root: Optional[TreeNode]) -> int:
+		self.dfs(root);
+		return self.max_path
+
+	def dfs(self,root):
+		if root is None:return 0
+		left=self.dfs(root.left)
+		right=self.dfs(root.right)
+
+		if root.left  and  root.left.val == root.val:
+			leftPath=left+1
+		else:
+			leftPath=0
+
+		if root.right and  root.right.val == root.val:
+			rightPath=right+1
+		else:
+			rightPath=0
+
+		self.max_path = max(self.max_path, leftPath + rightPath)
+		return max(leftPath, rightPath)
+    
