637931afbf5b5d042c80a61859248b6e094f306f
Lawrence Tsai <l.tsai@mail.utoronto.ca>
2022-08-09 16:47:39 -0600

    Create 32-Longest Valid Parentheses.py

---
+++ 32-Longest Valid Parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestValidParentheses(self, s):
+        stack = [0]
+        longest = 0
+
+        for cur in s:
+            if cur == "(":
+                stack.append(0)
+            else:
+                if len(stack) > 1:
+                    val = stack.pop()
+                    stack[-1] += val + 2
+                    longest = max(longest, stack[-1])
+                else:
+                    stack = [0]
+
+        return longest
+        
