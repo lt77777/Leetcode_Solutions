e85c23851f75f1fe4fa02e76c74dcfa7a396f5ae
Lawrence Tsai <l.tsai@mail.utoronto.ca>
2022-08-09 19:52:31 -0600

    Create 225-Implement Stack using Queues.py

---
+++ 225-Implement Stack using Queues.py
@@ -0,0 +1,26 @@
+class MyStack:
+
+    def __init__(self):
+        self.queue=deque()
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        for i in range(len(self.queue)-1):
+            self.push(self.queue.popleft())
+        return self.queue.popleft()
+
+    def top(self) -> int:
+        return self.queue[-1]
+
+    def empty(self) -> bool:
+        return len(self.queue)==0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
